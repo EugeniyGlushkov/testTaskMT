a5504d64fa7dc24584dd4acfd3e4b60c1c5ad14a
EugeniyGlushkov <eugeniyglushkov@gmail.com>
2019-03-21 10:18:09 +0300

    Add new User. RUN

--- src/main/java/ru/alvisid/testtaskmt/DatabaseLoader.java
+++ src/main/java/ru/alvisid/testtaskmt/DatabaseLoader.java
@@ -3,7 +3,9 @@ package ru.alvisid.testtaskmt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import ru.alvisid.testtaskmt.model.User;
 
+import java.time.LocalDate;
 import java.util.GregorianCalendar;
 
 @Component
@@ -18,9 +20,9 @@ public class DatabaseLoader implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
-        this.repository.save(new User("Paul", "paul@yandex.ru", new GregorianCalendar(2001, 5, 12)));
-        this.repository.save(new User("Jhon", "jhon@mail.ru", new GregorianCalendar(2002, 10, 25)));
-        this.repository.save(new User("Ann", "ann@list.ru", new GregorianCalendar(2003, 3, 1)));
-        this.repository.save(new User("Mary", "mary@gmail.com", new GregorianCalendar(2000, 2, 9)));
+        this.repository.save(new User(1, "Paul", "paul@yandex.ru", "paul", LocalDate.of(1995, 10, 22)));
+        this.repository.save(new User(2, "Jhon", "jhon@mail.ru", "jhon", LocalDate.of(2001, 5, 12)));
+        this.repository.save(new User(3, "Ann", "ann@list.ru", "ann", LocalDate.of(2003, 3, 1)));
+        this.repository.save(new User(4, "Mary", "mary@gmail.com", "mary", LocalDate.of(2000, 2, 9)));
     }
 }
--- src/main/java/ru/alvisid/testtaskmt/UserRepository.java
+++ src/main/java/ru/alvisid/testtaskmt/UserRepository.java
@@ -1,5 +1,6 @@
 package ru.alvisid.testtaskmt;
 
 import org.springframework.data.repository.CrudRepository;
+import ru.alvisid.testtaskmt.model.User;
 
-public interface UserRepository extends CrudRepository <User, Long> {}
+public interface UserRepository extends CrudRepository <User, Integer> {}
