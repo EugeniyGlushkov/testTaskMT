f88955ca3775e5900f71cfa85dce3b9adc18a48c
EugeniyGlushkov <eugeniyglushkov@gmail.com>
2019-03-21 12:44:09 +0300

    Add repository and move HomeController. RUN

--- src/main/java/ru/alvisid/testtaskmt/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package ru.alvisid.testtaskmt;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.alvisid.testtaskmt.model.User;
-
-public interface UserRepository extends CrudRepository <User, Integer> {}
--- src/main/java/ru/alvisid/testtaskmt/web/HomeController.java
+++ src/main/java/ru/alvisid/testtaskmt/web/HomeController.java
@@ -1,4 +1,4 @@
-package ru.alvisid.testtaskmt;
+package ru.alvisid.testtaskmt.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
