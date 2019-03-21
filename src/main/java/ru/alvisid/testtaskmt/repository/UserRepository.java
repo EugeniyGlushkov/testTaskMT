3e79967e31eaa36bb385a5d1daadd52a28673956
EugeniyGlushkov <eugeniyglushkov@gmail.com>
2019-03-21 16:51:53 +0300

    User repo change.RUN

--- src/main/java/ru/alvisid/testtaskmt/repository/UserRepository.java
+++ src/main/java/ru/alvisid/testtaskmt/repository/UserRepository.java
@@ -1,55 +1,73 @@
 package ru.alvisid.testtaskmt.repository;
 
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.alvisid.testtaskmt.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
- * The generalized functional for user's repository.
+ * JpaRepository interface for user.
  *
  * @author Glushkov Evgen
  * @version 1.0
  * @since 2019.03.15
  */
-public interface UserRepository {
+@Transactional(readOnly = true)
+public interface UserRepository extends JpaRepository <User, Integer> {
     /**
      * Saves or updates a given user.
+     * If there are a given object in the data base then the given object will be update.
+     * If there aren't a given object in the data base then a new object
+     * with new id (data base set default value) will be saved.
      *
-     * @param user the user to save or update.
-     * @return a saved or update user,
-     * null - if there aren't updated user in the data base.
+     * @param user a user to save.
+     * @return the saved user.
      */
+    @Override
+    @Transactional
     User save(User user);
 
     /**
-     * Deletes the user by specified id.
+     * Deletes a user by given id.
      *
-     * @param id the specified id of the deleted user.
-     * @return {@code true} - the entity is deleted, {@code false} - the entity isn't found.
+     * @param id id of the user that must be deleted.
+     * @return amount of the deleted entities.
      */
-    boolean delete(int id);
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM User u WHERE u.id=:id")
+    int delete(@Param("id") int id);
 
     /**
-     * Returns the user with the specified id.
+     * Returns a container with a user by the given id inside.
      *
-     * @param id the specified id of the user to get.
-     * @return the user with the specified id,
-     * null - if there aren't the user with the specified id in the DB.
+     * @param integer id of the user to return.
+     * @return a container with an user by the given id inside.
      */
-    User get(int id);
+    @Override
+    Optional <User> findById(Integer integer);
+
 
     /**
-     * Returns the list with all users.
+     * Returns all users sorted with a given sort.
      *
-     * @return the list with all users.
+     * @param sort the sort for users list.
+     * @return list of all users sorted with a given sort.
      */
-    List <User> getAll();
+    @Override
+    List <User> findAll(Sort sort);
 
     /**
-     * Returns the user by given email.
+     * Returns an user by the given email.
      *
      * @param email the specified email.
-     * @return the user by given email.
+     * @return the user by the given email.
      */
     User getByEmail(String email);
 }
